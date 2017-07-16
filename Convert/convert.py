902f20b923f1fd7e4c12e2abe27007044cff829a
Vadym Markov <markov.vadym@gmail.com>
2017-07-17 00:35:45 +0200

    Update conversion

--- Convert/convert.py
+++ Convert/convert.py
@@ -1,9 +1,9 @@
 import coremltools
 
 coreml_model = coremltools.converters.caffe.convert(('twitter_finetuned_test4_iter_180.caffemodel', 'sentiment_deploy.prototxt'), image_input_names = 'data', class_labels='labels.txt')
-coreml_model.author = 'Campos, V., Jou, B., & Giro-i-Nieto, X.'
+coreml_model.author = 'Image Processing Group - BarcelonaTECH - UPC'
 coreml_model.license = 'MIT'
-coreml_model.short_description = 'Fine-tuned CNN for Visual Sentiment Prediction'
+coreml_model.short_description = 'Fine-tuning CNNs for Visual Sentiment Prediction'
 coreml_model.input_description['data'] = 'An image.'
 coreml_model.output_description['prob'] = 'The probabilities for each sentiment, for the given input.'
 coreml_model.output_description['classLabel'] = 'The most likely sentiment, for the given input.'
--- Convert/download.sh
+++ Convert/download.sh
@@ -1,2 +1,2 @@
 wget https://imatge.upc.edu/web/sites/default/files/projects/affective/public_html/2017-imavis/twitter_finetuned_test4_iter_180.caffemodel
-wget https://github.com/imatge-upc/sentiment-2017-imavis/blob/master/sentiment_deploy.prototxt
+wget https://github.com/imatge-upc/sentiment-2017-imavis/raw/master/sentiment_deploy.prototxt
